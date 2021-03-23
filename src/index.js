1aabef0b9f644e420c39f79a83c8d3cb473d6e76
Lucas dos Santos <31446385+lucasSPro@users.noreply.github.com>
2021-03-22 23:20:44 -0300

    adding dependencies

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(express.json());
 
 const repositories = [];
 
-app.get("/repositories", (request, response) => {
+app.get("/repositories", (request, response) => {""
   return response.json(repositories);
 });
 
